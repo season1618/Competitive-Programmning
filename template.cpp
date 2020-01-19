0fee1e6db8e02ba151d19544285a3c616ca025f4
minority1618 <8643-mino-1035@ezweb.ne.jp>
2020-01-19 15:56:26 +0900

    add new file

---
+++ template.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define range(a) (a).begin(),(a).end()
+#define print(A,x,n) rep(i,0,n){cout<<(i ? " ":"")<<A[i]x;}cout<<endl;
+#define pprint(A,y,m,n) rep(j,0,m){print(A[j],y,n);}
+const long mod=1e5;
+const int size=1e5;
+const int inf=1e9;
+int main(){
+}
+/*タスクを並列に行う 二分探索
+プログラミングコンテストでの動的計画法
+最強最速アルゴリズマー養成講座
+sorted
+auto it=lower_bound(v.begin(),v.end(),w) w <= v[it-v.begin()]
+auto it=upper_bound(v.begin(),v.end(),w) w < v[it-v.begin()]
+void func(int* vec){}
+putchar(char) cout<<char<<endl;
+puts(string) cout<<string<<endl;
+1.*a/b int to double
+(int)'A'-65 char to int
+(int)'a'-48 char to int
+(char)(i+97) int to char
+stoi(s) string to int
+stoll(s)
+s={c} char to string
+s=string()+c
+s=""s+c
+cout.precision(16);
+cout<<fixed<<a;
+printf("%.6lf\n",a);
+accumulate(a,a+n,0);
+int A[N];
+rep(i,0,N) A[i]=i+1;
+do{
+}while(next_permutation(A,A+N));
+*/
