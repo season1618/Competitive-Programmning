d88e81de5ba25dc7933a84beac8c8d43ce286b57
minority1618 <8643-mino-1035@ezweb.ne.jp>
2020-02-08 01:45:20 +0900

    program

--- Graph-Theory/Dijkstra.cpp
+++ Graph-Theory/Dijkstra.cpp
@@ -3,26 +3,23 @@ using namespace std;
 const int size=1e5;
 const int inf=1e9;
 int N,M;
-vector<vector<pair<int,long>>> g(size);
-long d[size];int pre[size];
-void dijkstra(int a){//O(E+NlogN)
+vector<pair<int,long>> g[size];
+long d[size]; int prev[size];
+void dijkstra(int a){// O(NlogN+M)
 	priority_queue<pair<long,int>> pq;
-	rep(i,0,N){
-		d[i]=inf;
-		pq.push({-d[i],i});
-	}
-	d[a]=0;pq.push({0,a});
-	int v;long temp;
-	rep(i,0,N){
-		v=pq.top().second;
+	rep(i,0,N){ d[i] = inf; pq.push({-d[i],i}); }
+	d[a] = 0;pq.push({0,a});
+	int v; long temp;
+	while(!pq.empty()){
+		v = pq.top().second;
 		pq.pop();
         int u;
 		for(pair<int,long> p:g[v]){
-            u=p.first;
-			temp=d[v]+p.second;
-			if(d[u]>temp){
-				d[u]=temp;
-				pre[u]=v;
+            u = p.first;
+			temp = d[v] + p.second;
+			if(d[u] > temp){
+				d[u] = temp;
+				prev[u] = v;
 				pq.push({-d[u],u});
 			}
 		}
@@ -32,7 +29,7 @@ int main(){
 	cin>>N>>M;
 	int a,b;long c;
 	rep(i,0,M){
-		cin>>a>>b>>c;a--;b--;
+		cin>>a>>b>>c; a--;b--;
 		g[a].push_back({b,c});
 		g[b].push_back({a,c});
 	}
