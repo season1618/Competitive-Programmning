02f60d89b21e5140d81c1193feef6e57cf1f2ee2
season1618 <seasonminority314@gmail.com>
2021-10-02 23:40:48 +0900

    modified notations, move typename to class

--- mathematics/power.cpp
+++ mathematics/power.cpp
@@ -1,23 +1,18 @@
-#include<bits/stdc++.h>
-using namespace std;
-const long mod = 1e9+7
-template<typename T>
-T power(T x, T n){
-	ret = 1; T i = 0;T
-	while(n >= (1<<i)){
-		if(n & (1<<i)) ret *= x;
-		i++; x = x * x;
-	}return ret;
+template<class T> T power(T x, T exp){
+	T ret = 1;
+	while(exp){
+		if(exp % 2) (ret *= x) %= mod;
+		x = (x * x) % mod;
+		exp /= 2;
+	}
+	return ret;
 }
-long modpow(long x, int n, long p){
-	long ret = 1; int i = 0;
-	while(n >= (1<<i)){
-		if(n & (1<<i)) (ret *= x) %= p;
-		i++;
-		x = (x * x) % p;
-	}return ret;
-}
-int main(){
-	long x;int n;cin>>x>>n;
-	cout<<power(x,n)<<" "<<modpow(x,n,mod)<<endl;
+long modpow(long x, int exp, long mod){
+	long ret = 1;
+	while(exp){
+		if(exp % 2) (ret *= x) %= mod;
+		x = (x * x) % mod;
+		exp /= 2;
+	}
+	return ret;
 }
