5e135284b6a746a34661968ec47b876b6672866e
season1618 <seasonminority314@gmail.com>
2021-10-02 23:35:28 +0900

    abstract operation, add func(), at()

--- data-structure/segment-tree.cpp
+++ data-structure/segment-tree.cpp
@@ -48,27 +48,35 @@ public:
 
 class lazysegtree{
 public:
-    int n;
-	vector<int> node, lazy;
+	int n;
+	vector<long> node, lazy;
 	lazysegtree(int siz){
 		n = 1;
 		while(n < siz) n *= 2;
 		node.resize(2*n-1, 0);
 		lazy.resize(2*n-1, 0); // tanigen
 	}
+private:
+	long func(long x, long y){
+		return (x + y) % mod;
+	}
 	void eval(int v, int l, int r){
 		if(lazy[v] != 0){
-			node[v] = max(node[v], lazy[v]);
+			node[v] = func(node[v], lazy[v]);
 			if(r - l > 1){
-				lazy[2*v+1] = max(lazy[2*v+1], lazy[v]);
-				lazy[2*v+2] = max(lazy[2*v+2], lazy[v]);
+				lazy[2*v+1] = func(lazy[2*v+1], lazy[v]);
+				lazy[2*v+2] = func(lazy[2*v+2], lazy[v]);
 			}
 			lazy[v] = 0;
 		}
 	}
-	void update(int l, int r, int x, int v = 0, int a = 0, int b = -1){ // node[i] += x, i in [l, r)
+public:
+	long at(int v){
+		return func(node[v+n-1], lazy[v+n-1]);
+	}
+	void update(int l, int r, long x, int v = 0, int a = 0, int b = -1){ // node[i] += x, i in [l, r)
 		if(b == -1) b = n;
-      	eval(v, a, b);
+		eval(v, a, b);
 		if(b <= l || r <= a) return;
 		else if(l <= a && b <= r){
 			lazy[v] = x;
@@ -77,17 +85,17 @@ public:
 		else{
 			update(l, r, x, 2*v+1, a, (a+b)/2);
 			update(l, r, x, 2*v+2, (a+b)/2, b);
-			node[v] = max(node[2*v+1], node[2*v+2]);
+			node[v] = func(node[2*v+1], node[2*v+2]);
 		}
 	}
-	int query(int l, int r, int v = 0, int a = 0, int b = -1){
-        if(b == -1) b = n;
+	long query(int l, int r, int v = 0, int a = 0, int b = -1){
+		if(b == -1) b = n;
 		if(b <= l || r <= a) return 0;
 		eval(v, a, b);
 		if(l <= a && b <= r) return node[v];
 		int s = query(l, r, 2*v+1, a, (a+b)/2);
 		int t = query(l, r, 2*v+2, (a+b)/2, b);
-		return max(s, t);
+		return func(s, t);
 	}
 };
 
