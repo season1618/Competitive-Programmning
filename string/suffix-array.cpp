8eb669d009e573fb8397ab5b07f5ee39c9f080fd
season1618 <seasonminority314@gmail.com>
2021-10-24 18:52:30 +0900

    construct by doubling

--- string/suffix-array.cpp
+++ string/suffix-array.cpp
@@ -5,7 +5,7 @@ public:
     int n;
     string s;
     vector<int> sa, rank;
-    suffix_array(stirng s){
+    suffix_array(string s){
         this->n = s.size();
         this->s = s + "$";
         sa.resize(n+1);
@@ -13,33 +13,44 @@ public:
         for(int i = 0; i < n; i++){
             sa[i] = i;
         }
+        construct();
     }
 
 private:
-    int compare1(int i, int j, int lgt){
-        if(lgt == 0) return 0;
-        if(lgt == 1){
-            if(s[i] > s[j]) return -1;
-            else if(s[i] == s[j]) return 0;
-            else return 1;
+    void construct(){ // O(nlog^2n)
+        int len = 0;
+        int m = n;
+        while(m > 0){ m /= 2; len++; }
+        int vec[len][n+1];
+        for(int i = 0; i <= n; i++) vec[0][i] = s[i] - 'a';
+        for(int k = 0; k < len-1; k++){
+            map<pair<int,int>,int> mp;
+            for(int i = 0; i <= n; i++) mp[{vec[k][i], vec[k][(i + (1<<k)) % (n+1)]}] = 1;
+            int j = 0;
+            for(auto [p, r] : mp) mp[p] = j++;
+            for(int i = 0; i <= n; i++) vec[k+1][i] = mp[{vec[k][i], vec[k][(i + (1<<k)) % (n+1)]}];
         }
-        for(int k = 25; k >= 0; k--){
-            if((lgt >> k) ^ 1) continue;
-            int t = (1 << k);
-            switch(compare(i, j, t)){
-                case -1:
-                    return -1;
-                case 0:
-                    return compare((i+t) % (n+1), (j+t) % (n+1), lgt-t);
-                case 1:
-                    return 1;
+        pair<int,int> vp[n+1];
+        for(int i = 0; i <= n; i++) vp[i].second = i;
+        int sum = 0;
+        for(int k = 0; k < len; k++){
+            if((n + 1) & (1 << k)){
+                sum += (1 << k);
+                for(int i = 0; i <= n; i++){
+                    int j = vp[i].second;
+                    vp[i].first = vec[k][j+n+1-sum];
+                }
+                sort(vp,vp+n+1);
             }
         }
-    }
-    bool compare2(int i, int j){
-        return compare1(i, j, n) + 1;
-    }
-    void construct(){
-        sort()
+        for(int i = 0; i <= n; i++){
+            int j = vp[i].second;
+            sa[i] = j;
+            rank[j] = i;
+        }
     }
 };
+int main(){
+    string s = "hello";
+    suffix_array sa(s);
+}
