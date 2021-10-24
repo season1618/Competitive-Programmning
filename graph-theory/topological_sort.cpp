1fd40ff1e30e5ba55141a345cdd3dfd393e58d24
season1618 <seasonminority314@gmail.com>
2021-10-24 18:54:27 +0900

    modify notation

--- graph-theory/topological_sort.cpp
+++ graph-theory/topological_sort.cpp
@@ -1,16 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define rep(i,x,y) for(int i=x;i<y;i++)
-#define print(A,x,n) rep(I,0,n){cout<<(I ? " ":"")<<A[I]x;}cout<<endl;
-#define pprint(A,y,m,n) rep(J,0,m){print(A[J],y,n);}
-const long mod=1e9+7;
-const int size=1e5;
-const int inf=1e9;
+#define rep(i,x,y) for(int i=(int)(x);i<(int)(y);i++)
+#define print(A,x,n) {rep(I,0,n){cout<<(I ? " ":"")<<A[I]x;}cout<<endl;}
+#define pprint(A,y,m,n) {rep(J,0,m){print(A[J],y,n);}}
 int N,M;
 vector<int> g[size];
 int deg[size] = {};
-deque<int> ord; //deg == 0
-void tsort(){// O(M)
+deque<int> ord; // deg == 0
+void tsort(){ // O(M)
 	queue<int> ver;
 	rep(i,0,N)if(deg[i] == 0) ver.push(i);
 	while(!ver.empty()){
